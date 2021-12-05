d58f5349382ce68384da0db589e325f68e345e92
manbalboy <manbalboy@hanmail.net>
2021-12-06 00:36:34 +0900

    jest test 파일 삭제

--- stories/Button.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import Button from './Button.vue';
-
-describe('YourComponent', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Button);
-    expect(wrapper.vm).toBeTruthy();
-  });
-});
