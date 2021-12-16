70254b9ff2ac794d6686f91a2569cbb661d9eb5e
manbalboy <manbalboy@hanmail.net>
2021-12-16 22:08:54 +0900

    BaseCheckbox.spec.js 경로 수정 - #3

--- components/__tests__/inputs/BaseCheckbox.spec.js
+++ components/__tests__/inputs/BaseCheckbox.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
-import BaseCheckbox from '@/components/Inputs/BaseCheckbox.vue';
+import BaseCheckbox from '~/components/Inputs/BaseCheckbox.vue';
 
 describe('BaseCheckbox', () => {
   let wrapper;
