245838106749ff676f0adec9c12c9f1442c1717f
manbalboy <manbalboy@hanmail.net>
2021-12-16 23:04:13 +0900

    IconCheckbox /storybook 적용 컴포넌트 사용 안함 - #3
    추후 삭제 논의

--- components/Inputs/IconCheckbox.vue
+++ components/Inputs/IconCheckbox.vue
@@ -1,16 +1,6 @@
 <template>
-  <div
-    class="choice"
-    :class="{ active: checked }"
-    data-toggle="wizard-checkbox"
-    @click="updateValue"
-  >
-    <input
-      type="checkbox"
-      :name="name"
-      :disabled="disabled"
-      :checked="checked"
-    />
+  <div class="choice" :class="{ active: checked }" data-toggle="wizard-checkbox" @click="updateValue">
+    <input type="checkbox" :name="name" :disabled="disabled" :checked="checked" />
     <div class="icon">
       <slot name="icon"> <i :class="icon"></i> </slot>
     </div>
@@ -20,26 +10,58 @@
   </div>
 </template>
 <script>
-export default {
-  name: 'icon-checkbox',
-  model: {
-    prop: 'checked'
-  },
-  props: {
-    checked: {
-      type: Boolean,
-      default: false
+  export default {
+    name: 'IconCheckbox',
+    model: {
+      prop: 'checked',
     },
-    name: String,
-    title: String,
-    icon: String,
-    disabled: Boolean
-  },
-  methods: {
-    updateValue() {
-      this.$emit('input', !this.checked);
-    }
-  }
-};
+    props: {
+      /**
+       * v-model value 값
+       */
+      checked: {
+        type: Boolean,
+        default: false,
+      },
+
+      /**
+       * checkbox name 속성
+       */
+      name: {
+        type: String,
+        default: '',
+      },
+
+      /**
+       * 아이콘 체크박스의 타이틀
+       */
+      title: {
+        type: String,
+        default: '',
+      },
+
+      /**
+       * default slot 의 icon class
+       */
+      icon: {
+        type: String,
+        default: '',
+      },
+
+      /**
+       * 비활성화 여부
+       */
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+    },
+
+    methods: {
+      updateValue() {
+        this.$emit('input', !this.checked);
+      },
+    },
+  };
 </script>
 <style></style>
